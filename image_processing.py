6c835c5986d501c83ff960f13fc80834c3e36420
sz <sebastianzaun95@gmail.com>
2022-09-25 18:38:48 +0200

    Speed up Patch Distance.

--- image_processing.py
+++ image_processing.py
@@ -1,101 +1,41 @@
 from re import I
-from numba import jit
+from numba import njit
 import numpy as np
 import matplotlib.pyplot as plt
 import random as rand
 import ot
-import math
 import time
 
-
-#------Function to calculate relabeling costs-------------------------------------------------------------------------------
-@jit(nopython=True, fastmath=True, parallel=True)
-def rcosts(arr1, arr2):
-    f1 = arr1.flatten()
-    f2 = arr2.flatten()
-
-    rc=np.zeros(4)      # Initial relabeling costs = 0
-    
-    # for (i,j) in zip(f1,f2):
-    #     if i != j:
-    #         rc+=1
-
-    for i in range(0,len(f1), 4):
-        rc[i]+=abs(f1[i]-f2[i])
-        rc[i+1]+=abs(f1[i+1]-f2[i+1])
-        rc[i+2]+=abs(f1[i+2]-f2[i+2])
-        rc[i+3]+=abs(f1[i+3]-f2[i+3])
-        rc[i+4]+=abs(f1[i+4]-f2[i+4])
-        rc[i+5]+=abs(f1[i+5]-f2[i+5])
-        rc[i+6]+=abs(f1[i+6]-f2[i+6])
-        rc[i+7]+=abs(f1[i+7]-f2[i+7])
-        
-
-    # for i in range(len(arr1)):        
-    #     for j in range(len(arr1[0])):
-    #         if arr1[i][j] != arr2[i][j]:
-    #             rc+=1     # Add up relabeling costs if entries in array are different
-                
-    return sum(rc)
-
-
-#------Function for rotating and flipping-----------------------------------------------------------------------------------
-def rf_costs(arr1,arr2,bool):   # bool for activating of rotating and flipping
-    
-    costs = []     # List for storing rcosts of all orientations
-
-    costs.append(rcosts(arr1,arr2))                               # Default orientation
-    
-    if bool:
-        costs.append(rcosts(np.flipud(arr1),arr2))                # Flip up and down       
-        costs.append(rcosts(np.rot90(arr1, 1),arr2))              # Rotate 90 degrees left
-        costs.append(rcosts(np.flipud(np.rot90(arr1,1)),arr2))    # Flip up and down + rotate 90 degrees left
-        costs.append(rcosts(np.rot90(arr1, 2),arr2))              # Rotate 180 degrees left
-        costs.append(rcosts(np.flipud(np.rot90(arr1,2)),arr2))    # Flip up and down + rotate 180 degrees left
-        costs.append(rcosts(np.rot90(arr1, 3), arr2))             # Rotate 270 degrees
-        costs.append(rcosts(np.flipud(np.rot90(arr1,3)),arr2))    # Flip up and down + rotate 270 degrees left
-    
-    return min(costs)      # Return the minimum rcosts of all orientations
+#------Faster function for calculation relabeling costs of all orientations-------------------------------------------------
+@njit(fastmath=True)
+def diff(arr1,arr2):
+    dim = len(arr1)
+    d=np.zeros(8)
+    for i in range(dim):
+        for j in range(len(arr1[0])):
+            d[0] += np.abs(arr1[i][j]-arr2[i][j])               # default
+            d[1] += np.abs(arr1[dim-j-1][i]-arr2[i][j])         # rot 90 left
+            d[2] += np.abs(arr1[dim-i-1][dim-j-1]-arr2[i][j])   # rot 180 left
+            d[3] += np.abs(arr1[j][dim-i-1]-arr2[i][j])         # rot 270 left
+            d[4] += np.abs(arr1[i][dim-j-1]-arr2[i][j])         # default flipped
+            d[5] += np.abs(arr1[dim-j-1][dim-i-1]-arr2[i][j])   # rot 90 left flipped
+            d[6] += np.abs(arr1[dim-i-1][j]-arr2[i][j])         # rot 180 left flipped
+            d[7] += np.abs(arr1[j][i]-arr2[i][j])               # rot 270 left flipped
+    return np.min(d)
 
 
 #------Function to divide images into patches---------------------------------------------------------------------------
-def get_patches(image, patchsize):
-    
-    mod = len(image) % patchsize
-    start = int(0 + mod / 2)
-    end = int(len(image) - mod / 2)
-    
-    
-    patches = []        # Empty list for storing all patches of the input image
-    
-    # for i in range(start, end - patchsize + 1):
-    #     for j in range(start, end - patchsize + 1):
-    #         patches.append(image[i:i+patchsize,j:j+patchsize])  
-    
-    for i in range(len(image)-patchsize+1):
+def get_patches(image, patchsize, step):
+    patches = []        # Empty list for storing all patches of the input image  
+    for i in range(0,len(image)-patchsize+1,step):
         for j in range(len(image[0])-patchsize+1):
-            patches.append(image[i:i+patchsize,j:j+patchsize])     # Appending all possible patches
-            
-    return patches
-
-
-#------Function to divide images into patches w/o intersection--------------------------------------------------------------
-def get_patches_wo_intersection(image, patchsize):
-    
-    patches = []        # Empty list for storing all patches of the input image
-    
-    for i in range(3):
-        for j in range(3):
-            patches.append(image[2 + i * patchsize : 2 + i * patchsize + 8, 2 + j * patchsize : 2 + j * patchsize + 8])
-    
+            patches.append(image[i:i+patchsize,j:j+patchsize])     # Appending all possible patches        
     return patches
 
 
 #------Function for approximating GED---------------------------------------------------------------------------------------
 def app_ged(arr1,arr2,bool):
-    
-    ged = rf_costs(arr1,arr2,bool)       # Approximate GED through rf_costs --> Add more steps (cut horizontal/vertical, cut tiles)?
-    
+    ged = diff(arr1,arr2)
     return ged
 
 
@@ -103,7 +43,7 @@ def app_ged(arr1,arr2,bool):
 def gonzalez_patches(patches, k, bool):
     
     heads = []                  # List for cluster heads
-    heads_DM = np.zeros((k,k))  # Distance matrix for distances between heads
+    heads_DM = np.zeros((k,k)).astype(int)  # Distance matrix for distances between heads
     cluster = []                # List for cluster IDs
     distributions = []          # List for the distributions of all images --> How many patches of the image belong to which cluster
     dist_to_head = []           # List to store the distance of each patch to its cluster's head
@@ -113,13 +53,13 @@ def gonzalez_patches(patches, k, bool):
 
     
     for i in range(len(patches)):
-        cluster.append(np.zeros(len(patches[i])))   # For every image i append zero array of length: # patches in one image
+        cluster.append(np.zeros(len(patches[i])).astype(int))   # For every image i append zero array of length: # patches in one image
     
     for i in range(len(patches)):
         dist_to_head.append([])     # For every image i append list for distances of patches to its cluster's head
                 
     for i in range(len(patches)):       # Create distributions for each image
-        distr = np.zeros(k)
+        distr = np.zeros(k).astype(int)
         distr[0] = len(patches[i])      # Starting with all the mass at the first cluster
         distributions.append(distr)
         
@@ -148,7 +88,6 @@ def gonzalez_patches(patches, k, bool):
                     max_ic_dist = max(dist_to_head[j])
                     image_index = j
                     patch_index = dist_to_head[j].index(max_ic_dist)
-        cluster[image_index][patch_index] = i + 1
         heads.append(patches[image_index][patch_index])
 
         for l in range(len(patches)):
@@ -163,7 +102,6 @@ def gonzalez_patches(patches, k, bool):
         time_end = time.time()
         print('Laufzeit:' + str(time_end - time_start))
                     
-    print('----------------------------------------------------------------------------------------------------')
     print('----------------------------------------------------------------------------------------------------')
     print('Heads Distance Matrix calculated:')
     
@@ -198,10 +136,10 @@ def plot_clustering(patches, cluster, k_start, k_end, split, name):
                     plt.axis('off')
                     cluster_counter[i] += 1
                 else:
-                    fig.add_subplot(k, cluster_size, (i - k * split) * cluster_size + int(cluster_counter[i - k * split] + 1))
+                    fig.add_subplot(k, cluster_size, (i - k) * cluster_size + int(cluster_counter[i - k] + 1))
                     plt.imshow(patches[j])
                     plt.axis('off')
-                    cluster_counter[i - k * split] += 1
+                    cluster_counter[i - k] += 1
         else:
             for j in range(cluster_size):
                 if k_start == 0:
@@ -212,26 +150,44 @@ def plot_clustering(patches, cluster, k_start, k_end, split, name):
                     cluster_counter[i] += 1
                 else:
                     l = rand.choice(indices[0])
-                    fig.add_subplot(k, cluster_size, (i - k * split) * cluster_size + int(cluster_counter[i - k * split] + 1))
+                    fig.add_subplot(k, cluster_size, (i - k) * cluster_size + int(cluster_counter[i - k] + 1))
                     plt.imshow(patches[l])
                     plt.axis('off')
-                    cluster_counter[i - k * split] += 1
+                    cluster_counter[i - k] += 1
                     
     plt.subplots_adjust(0,0,1,1,0.1,0.1)
     plt.savefig(name+".png")
     #plt.show()
     
     
+#------Plot Clustering------------------------------------------------------------------------------------------------------
+def plot_clustering2(patches, cluster, k_start, k_end, name):
+    k = k_end - k_start
+    cluster_size = 30
+    indices = []
+    fig = plt.figure(figsize=(cluster_size * 8, k * 8))
+    row_counter = 0
+    column_counter = 0
+    for i in range(k_start, k_end):
+        indices.append(np.where(cluster == i))
+    for i in indices:
+        l = rand.choice(i, cluster_size)
+        for j in range(len(l)):
+            fig.add_subplot(k, cluster_size, row_counter * cluster_size + column_counter)
+            plt.imshow(patches[l[j]])
+            column_counter += 1
+        row_counter += 1
+    plt.subplots_adjust(0,0,1,1,0.1,0.1)
+    plt.savefig(name+".png")
+    
+
 #------Function to compute Wasserstein Distance-----------------------------------------------------------------------------
 def wasserstein_dist(distr1, distr2, CM):
-    
     wd = 0
-    
     if len(distr1) == len(distr2) and len(distr1) == len(CM):
         wd = ot.emd2(distr1, distr2, CM)
     else:
         print('Error WD computation!')    
-    
     return wd
 
 
@@ -241,7 +197,7 @@ def gonzalez_images(k, distributions, CM):
     heads = [] 
     heads.append(distributions[0])
     dist_to_head = []
-    cluster = np.zeros(len(distributions))
+    cluster = np.zeros(len(distributions)).astype(int)
     initial = True
     
     print('----------------------------------------------------------------------------------------------------')
@@ -276,7 +232,7 @@ def gonzalez_images(k, distributions, CM):
 #------Image Clustering based on a k-median Clustering----------------------------------------------------------------------
 def d2_images(k1, distributions, CM, stop):
     
-    cluster = np.zeros(len(distributions))
+    cluster = np.zeros(len(distributions)).astype(int)
     center = np.zeros(k1).astype(int)
     ic_dist = np.zeros(k1)
     start = np.random.randint(0, len(distributions), k1)
@@ -290,8 +246,8 @@ def d2_images(k1, distributions, CM, stop):
 
     for i in range(stop):
         print('Iteration: ' + str(i + 1) + ' of ' + str(stop))
+        print('Assignment to cluster.')
         for j in range(len(distributions)):
-            print('Iteration ' + str(i) + ':\tAssignment\t' + str(j + 1) + '\tof\t' + str(len(distributions)) + '.')
             initial = True
             temp1 = 0
             for k in range(len(center)):
@@ -312,10 +268,7 @@ def d2_images(k1, distributions, CM, stop):
             index = 0
             for k in indices[0]:
                 temp = 0
-                #counter = 1
                 for l in indices[0]:
-                    #print('Calculation of Centroid ' + str(j + 1) + ': ' + str(counter) + ' of ' + str(len(indices[0]) * len(indices[0])))
-                    #counter += 1
                     temp += wasserstein_dist(distributions[k], distributions[l], CM)
                 if initial:
                     ic_dist = temp
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from importlib.metadata import distributions
 from pickletools import uint8
 from keras.datasets import mnist
 import numpy as np
@@ -84,24 +85,63 @@ def main():
     for i in sample:
         images.append(i.astype(int))
     k_images = 8
-    k_patches = 3
+    k_patches = 100
     bool_rot = True
-    patchsize = 100
+    patchsize = 8
     patches = []
-    # for i in images:
-    #     patches.append(ip.get_patches(i, patchsize))
-    # heads, heads_CM, distributions, cluster_patches = ip.gonzalez_patches(patches, k_patches, bool_rot)
-    # cluster_images, center_images = ip.d2_images(k_images, distributions, heads_CM, 20)
-    # ip.plot_clustering(images, cluster_images, 0, k_images, 0, "TEST")
-    start = time.time()
-    print(ip.app_ged(images[0], images[1], True))
-    end = time.time()
-    print(end - start)
-    start = time.time()
-    print(ip.app_ged(images[0], images[1], True))
-    end = time.time()
-    print(end - start)
-        
+    for i in images:
+        patches.append(ip.get_patches(i, patchsize,1))
+    heads, heads_CM, distributions, cluster_patches = ip.gonzalez_patches(patches, k_patches, bool_rot)
+    # patches_flat = []
+    # cluster_flat = np.zeros(len(patches) * len(patches[0]))
+    # counter = 0
+    # for i in range(len(patches)):
+    #     for j in range(len(patches[0])):
+    #         patches_flat.append(patches[i][j])
+    #         cluster_flat[counter] = cluster_patches[i][j]
+    #         counter += 1
+    # ip.plot_clustering(patches_flat, cluster_flat, 0, 50, 2, "TEST_PATCH_CLUSTERING1")
+    # ip.plot_clustering(patches_flat, cluster_flat, 50, 100, 2, "TEST_PATCH_CLUSTERING2")
+    cluster_images_gonzalez = ip.gonzalez_images(k_images, distributions,heads_CM)
+    cluster_images, center_images = ip.d2_images(k_images, distributions, heads_CM, 20)
+    print(len(cluster_images_gonzalez) == len(cluster_images))
+    cluster_counter_gonzalez = np.zeros(8)
+    cluster_counter_d2 = np.zeros(8)
+    for i in range(k_images):
+        cluster_counter_gonzalez[i] = np.count_nonzero(cluster_images_gonzalez == i)
+        cluster_counter_d2[i] = np.count_nonzero(cluster_images == i)
+    print('Gonzalez:')
+    print(cluster_counter_gonzalez)
+    print()
+    print('D2:')
+    print(cluster_counter_d2)
+
+
+    ip.plot_clustering2(images, cluster_images, 0, k_images, "TEST_D2")
+    ip.plot_clustering2(images,cluster_images_gonzalez, 0, k_images, "TEST_GONZALEZ")
+
+    # test_patches = []
+    # A = np.array([[1,1,1,1,1,1,1,1],[1,0,0,0,0,0,0,1],[1,0,0,0,0,0,0,1],[1,0,0,0,0,0,0,1],\
+    #     [1,0,0,0,0,0,0,1],[1,0,0,0,0,0,0,1],[1,0,0,0,0,0,0,1],[1,1,1,1,1,1,1,1]])
+    # B = np.array([[0,0,0,0,0,0,0,0],[0,1,1,1,1,1,1,0],[0,1,0,0,0,0,1,0],[0,1,0,0,0,0,1,0],\
+    #     [0,1,0,0,0,0,1,0],[0,1,0,0,0,0,1,0],[0,1,1,1,1,1,1,0],[0,0,0,0,0,0,0,0]])
+    # C = np.array([[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,1,1,1,1,0,0],[0,0,1,0,0,1,0,0],\
+    #     [0,0,1,0,0,1,0,0],[0,0,1,1,1,1,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0]])
+    # D = np.array([[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,1,1,0,0,0],\
+    #     [0,0,0,1,1,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0]])
+            
+    # tmp = []
+    # for i in range(3):
+    #     tmp.append(A)
+    #     tmp.append(B)
+    #     tmp.append(C)
+    #     tmp.append(D)
+    # test_patches.append(tmp)
+
+    # test_heads, test_heads_CM, test_distributions, test_cluster_patches = ip.gonzalez_patches(test_patches, 4, True)
+    # ip.plot_clustering(test_patches, test_cluster_patches, 0, 4, 0, "TEST_PATCHES")
+    # print(test_distributions)
+
 # Run main
 if __name__ == "__main__":
     main()
